3124283ad5c95a506aa7a858de55112c5a9ae0b1
R41ndrop12 <sanyamshekhawat07@gmail.com>
2023-10-11 17:58:01 -0400

    Pre-Save Files
    Saving just before I rework everything so I can add save files.

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -7,16 +7,12 @@ public class BallManager : MonoBehaviour
 {
     private ProbabilityManager pm;
     private MoneyManager mm;
-    private Transform gm;
     public float money = 1f;
-    private float[] multiplier = { 0.5f, 1.25f, 1.75f, 3f, 5f, 10f, 25f, 50f, 100f };
-    public float offset = 4f;
     // Start is called before the first frame update
     void Start()
     {
         pm = FindObjectOfType<ProbabilityManager>();
         mm = FindObjectOfType<MoneyManager>();
-        gm = this.gameObject.transform;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -11,7 +11,7 @@ public class MoneyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddMoney(100000f);
     }
 
     // Update is called once per frame
